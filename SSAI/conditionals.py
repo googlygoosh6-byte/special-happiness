e04514509c0317832dcbcfc33569200c7c31a823
googlygoosh6-byte <googlygoosh6@gmail.com>
2025-09-29 09:02:25 +0000

    Python projects

---
+++ SSAI/conditionals.py
@@ -0,0 +1,6 @@
+# If statements governs conditional execution of code blocks.
+var = input("Enter True or False: ") # Prompt user for input
+if var == "True":
+    print("This block executes if var is True.")
+elif var == "False":
+    print("This block executes if var is False.")
